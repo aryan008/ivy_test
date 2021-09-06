dd942041daa29fa292be606bb5f2e7022ad4031d
aryan008 <adam.s.ryan@pwc.com>
2021-09-06 15:24:24 +0000

    CRUD start

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import *
+from .forms import OrderForm
 
 # Create your views here.
 
@@ -37,3 +38,9 @@ def customer(request, pk_test):
     context = {'customer':customer, 'orders':orders, 'order_count':order_count}
 
     return render(request, "accounts/customer.html", context)
+
+def createOrder(request):
+    form = OrderForm()
+
+    context = {'form':form}
+    return render(request, "accounts/order_form.html", context)
