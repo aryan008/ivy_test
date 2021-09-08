63c11e988c478966e4039acacc4319247c21bffc
aryan008 <adam.s.ryan@pwc.com>
2021-09-08 13:17:30 +0000

    filter form table search

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.forms import inlineformset_factory
 
 from .models import *
 from .forms import OrderForm
+from .filters import OrderFilter
 
 # Create your views here.
 
@@ -31,12 +32,14 @@ def products(request):
     return render(request, "accounts/products.html", {'products':products})
 
 def customer(request, pk_test):
-
     customer = Customer.objects.get(id=pk_test)
     orders = customer.order_set.all()
     order_count = orders.count()
 
-    context = {'customer':customer, 'orders':orders, 'order_count':order_count}
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
+    context = {'customer':customer, 'orders':orders, 'order_count':order_count, 'myFilter':myFilter}
 
     return render(request, "accounts/customer.html", context)
 
